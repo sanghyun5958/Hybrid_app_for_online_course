b98afe9f423ba4be79e76e2f8763d7043d8f8b16
sanghyunj5958 <sanghyun5958@gmail.com>
2018-04-01 17:26:15 +0900

    Initial commit

---
+++ assets/www/story_content/user.js
@@ -0,0 +1,306 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xhUurMjd3v":
+        Script1();
+        break;
+      case "6AVzGjolX0A":
+        Script2();
+        break;
+      case "6PdBPnjhP5t":
+        Script3();
+        break;
+      case "6NNOYKa08gu":
+        Script4();
+        break;
+      case "5fX56TVjW1o":
+        Script5();
+        break;
+      case "6C6NNvsRUjk":
+        Script6();
+        break;
+      case "6cfXNjfPUFi":
+        Script7();
+        break;
+      case "5o1g0HgKFK8":
+        Script8();
+        break;
+      case "6945bYOYcog":
+        Script9();
+        break;
+      case "5klyDyxNGL5":
+        Script10();
+        break;
+      case "6ra8Id1EKqL":
+        Script11();
+        break;
+      case "5bqWFcj65dg":
+        Script12();
+        break;
+      case "5wEWlwMoOoo":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script2()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script3()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script4()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script5()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script6()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script7()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script8()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script9()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script10()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script11()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script12()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
+function Script13()
+{
+  document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
+
+}
+
